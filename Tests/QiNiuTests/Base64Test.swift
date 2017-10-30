148bd239c3cd93cea3668eac15ccf3e1d06a9ba7
YourtionGuo <yourtion@gmail.com>
2017-10-30 16:55:52 +0800

    update Base64Test

--- Tests/QiNiuTests/Base64Test.swift
+++ Tests/QiNiuTests/Base64Test.swift
@@ -5,12 +5,12 @@ class Base64Tests: XCTestCase {
 
   func testEncodeString() {
     let s = "你好/+=";
-    XCTAssert(s.base64SafeEncode() == "5L2g5aW9Lys9", "Pass");
+    XCTAssertEqual(s.base64SafeEncode(), "5L2g5aW9Lys9");
   }
 
   func testDecodeString() {
     let s = "5L2g5aW9Lys9";
-    XCTAssert(String(data: s.base64SafeDecode()!, encoding: .utf8) == "你好/+=", "Pass");
+    XCTAssertEqual(String(data: s.base64SafeDecode()!, encoding: .utf8), "你好/+=");
   }
   
 }
